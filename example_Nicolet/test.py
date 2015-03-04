105487bfbb4fb8fb07ef3de5426af8e56a39894d
aleaf <andrew.t.leaf@gmail.com>
2015-03-04 12:00:02 -0600

    added Medford example

---
+++ example_Nicolet/test.py
@@ -0,0 +1,21 @@
+__author__ = 'aleaf'
+
+import sys
+#sys.path.append('D:/ATLData/Documents/GitHub/GFLOW_utils')
+sys.path.append('..')
+import lsmaker as lsm
+import GISio
+from diagnostics import *
+import os
+
+
+ls = lsm.linesinks('Nicolet_lines.xml')
+
+ls.preprocess_arcpy()
+
+ls.preprocess()
+
+ls.makeLineSinks()
+
+ls.run_diagnostics()
+
