a43cb411e698fb446419dc7ae9f3661fd6b3413c
Leaf <aleaf@usgs.gov>
2016-02-02 11:09:59 -0600

    surfer grid class

--- lsmaker/utils/GFLOWresults.py
+++ lsmaker/utils/GFLOWresults.py
@@ -48,8 +48,62 @@ def write_streamflow_shapefile(xtr, outshp=None, solver_x0=0, solver_y0=0,
     if outshp is None:
         outshp = '{}_streamflow.shp'.format(xtr[:-4])
     df = get_linesink_results(xtr)
-    
+    df = df[df.resistance.values > 0].copy() # only include
     df[['x1', 'x2']] = df[['x1', 'x2']] * coords_mult + solver_x0
     df[['y1', 'y2']] = df[['y1', 'y2']] * coords_mult + solver_y0
     df['geometry'] = [LineString([(r.x1, r.y1), (r.x2, r.y2)]) for i, r in df.iterrows()]
     df2shp(df, outshp, epsg=epsg)
+
+class surferGrid:
+    def __init__(self, grdfile=None, data=None):
+        self.header = None
+        self.nrow = None
+        self.ncol = None
+        self.xmin = None
+        self.xmax = None
+        self.ymin = None
+        self.ymax = None
+        self.zmax = None
+        self.zmin = None
+        self.data = data
+        if grdfile is not None:
+            self.read_grd(grdfile)
+
+    def _read_grd_header(self, grdfile):
+        with open(grdfile) as input:
+            self.header = input.next()
+            self.nrow, self.ncol = map(int, input.next().strip().split())
+            self.xmin, self.xmax = map(float, input.next().strip().split())
+            self.ymin, self.ymax = map(float, input.next().strip().split())
+            self.zmin, self.zmax = map(float, input.next().strip().split())
+
+    def scale_xy(self, mult=1):
+        self.xmin *= mult
+        self.xmax *= mult
+        self.ymin *= mult
+        self.ymax *= mult
+
+    def scale_z(self, mult=1):
+        self.zmin *= mult
+        self.zmax *= mult
+
+    def offset_xy(self, x0=0, y0=0):
+        self.xmin += x0
+        self.xmax += x0
+        self.ymin += y0
+        self.ymax += y0
+
+    def read_grd(self, grdfile):
+        self._read_grd_header(grdfile)
+        data = np.loadtxt(grd, skiprows=5)
+        self.data = np.reshape(data, (self.ncol, self.nrow))
+
+    def write_grd(self, fname='output.grd'):
+
+        with open(fname, 'w') as output:
+            header = self.header.strip() + '\n'
+            header += '{:.0f} {:.0f}\n'.format(self.nrow, self.ncol)
+            header += '{:.2f} {:.2f}\n'.format(self.xmin, self.xmax)
+            header += '{:.2f} {:.2f}\n'.format(self.ymin, self.ymax)
+            header += '{:.2f} {:.2f}'.format(self.zmin, self.zmax)
+            np.savetxt(output, self.data, fmt='%.2f', delimiter=' ', header=header, comments='')
