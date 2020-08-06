32071a9ff37e32e733e1e6843c3e2b1aa92d3027
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-08-06 12:51:14 +0000

    Update docs after building Travis build 21 of aleaf/linesink-maker
    The docs were built from the branch 'master' against the commit
    3b5dbc93913a62a62cac43aff14eac7511d326b8.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/aleaf/linesink-maker/jobs/369345628.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test/bin/doctr deploy --built-docs docs/build/html . --branch-whitelist master develop

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.post0.dev0+g61e6704',
+    VERSION: '0.1.post0.dev0+g3b5dbc9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
