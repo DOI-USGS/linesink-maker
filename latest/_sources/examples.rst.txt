601dccd4d60cdfcc72bc5780b2fbc2bf59e869da
aleaf <andrew.t.leaf@gmail.com>
2023-11-28 03:54:01 +0000

    Deploying to gh-pages from  @ 2862a2d7c11a0df5eac1716f4c44685dba2b6e69 🚀

--- latest/_sources/examples.rst.txt
+++ latest/_sources/examples.rst.txt
@@ -7,4 +7,5 @@ Examples
    :maxdepth: 1
 
    Medford National Forest Unit Example <example_medford>
+   Nicolet National Forest Unit Example <example_nicolet>
    Exporting and Visualizing GFLOW output <notebooks/GFLOW_results_visualization.ipynb>
--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,12 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.2.post10.dev0+g8b75d86',
-    LANGUAGE: 'None',
+    VERSION: '0.1.5.post4.dev0+g2862a2d',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
